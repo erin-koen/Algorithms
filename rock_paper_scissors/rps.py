ede847f47c05fe02cd29bcb321655f7f5bb4fc88
Erin Koen <erin.koen@gmail.com>
2019-06-06 16:18:27 -0400

    notes on rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python
 
+"""
+Understand
+----------
+- takes in an integer (though could control for that using math.floor) which represents how many players are in the game 
+- outputs a list of lists of strings. len(outer_list) = total number of possible combinations Inner lists are combinations of 'rock' 'paper' and 'scissor' and len(inner_list) = n
+- three possible outcomes for each 1 of n. Number of total possible outcomes is equal to 3^n.
+- return should be [[''],[''],[']...]
+- each play can be any of r, p, s. What you play on the next play not controlled by what you played on the last play. number of plays = n.
+
+
+Plan 
+----
+Create an outer function that houses the list to be returned and the list of 'rock' 'paper' and 'scissors
+Recursion. 
+
+Execute
+-------
+
+Analyze
+-------
+
+"""
+
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  possibilities = 3**n
+  return_list= possibilities*[[0]]
+  rps = ['rock', 'paper', 'scissors']
+  print([[return_list] + [rps]])
+  # def rps_recur(n):0
+    # base case: 
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+rock_paper_scissors(3)
