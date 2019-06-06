c6f9949fb9b7ea7a4f52120ca700ad4651fb61a2
Erin Koen <erin.koen@gmail.com>
2019-06-06 14:39:20 -0400

    Thursday branch

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python
 
+"""
+Understand
+----------
+- passed an integer
+- four different ways to subtract from that integer. 0, 1, 2, 3. Does 0 matter?
+  - turns out yes, as zero is tested in line 7 haha. Conditional then, if n = zero then return 1
+- always integers?
+- Base case - n <= 1? There's only one way to eat one cookie. 
+- 
+ 
+
+Plan 
+----
+- recursive case (naive). Call the function on N, return 1 if N == 0, return N if N is 1 or 2. Return function on each of the possibilities for subtraction, i.e. 1, 2, 3. So return eating_cookings(n-1) + eating_cookies(n-2) + eating_cookies(n-3). 
+
+Execute
+-------
+
+
+Analyze
+-------
+
+
+"""
+
 import sys
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
-
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+  if n == 0:
+    return 1
+  elif n <=2:
+    return n
   else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    return eating_cookies(n-1)+eating_cookies(n-2)+eating_cookies(n-3)
